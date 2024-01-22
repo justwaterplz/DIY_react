78e64677929e8423f70d3fc8e2fe45c643e5c2cd
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2024-01-22 20:28:04 +0900

    layout

---
+++ capstone-login_prototype/src/Login.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+  // const [count, setCount] = useState(0)
+
+    return (
+        <div className="min-h-screen flex">
+            <div className="bg-yellow-300 flex-grow w-1/2">
+                <p>elldow?</p>
+            </div>
+
+            <div className="flex-grow w-1/2 bg-blue-300">
+                <p>blue?</p>
+            </div>
+        </div>
+    );
+}
+
+export default Login
