2ebb8cec2e2ca77e895bb4d2f62af47de57fad39
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2024-01-28 06:51:13 +0900

    update
    inputs can be entered with enter-key
    added option button's drop down menu
    fixed Message.jsx props error

--- capstone-login_prototype/src/Chat.jsx
+++ capstone-login_prototype/src/Chat.jsx
@@ -1,18 +1,23 @@
-import Message from "./components/Message";
+import Message from './components/Message';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 //채팅창 화면 구현
 const Chat = () => {
 
-    //hard coded messages for testing.
-    // const messages = [
-    //     { text: 'hello world1', sender: 'me' },
-    //     { text: 'hello world2', sender: 'me' },
-    // ];
-
     const [msg, setMsg] = useState([]);
     const [newMessage, setNewMessage] = useState('');
+    //옵션 버튼
+    const [showOptions, setOptions] = useState(false);
 
+    const toggleOptions = () => {
+        setOptions(!showOptions);
+    }
+
+    const handleOptionClicks = (options) => {
+        alert(`thank you: ${options}`);
+    }
+    
     //message handler
     const handleMessages = () => {
         if (newMessage.trim() !== '') {
@@ -23,16 +28,27 @@ const Chat = () => {
         }
     }
 
+    //use enter key in keyboard to finish entering text.
+    const handleEnter = (e) => {
+        if(e.key === 'Enter'){
+            e.preventDefault();
+            //handling message in previous way
+            handleMessages();
+        }
+    }
+
+    
+
 
     return(
         //flex container
-        <div className="flex-col h-screen w-full">
+        <div className="flex-col min-h-screen w-full overflow-hidden">
             {/* header */}
-            <div className="w-screen h-28">
+            <div className="w-full h-28">
                 {/* pressing this button leads to welcome page or login page */}
-                <button className="h-28 border-solid border-teal-200 border-2">
+                <Link to="/login" className="h-28 border-solid border-teal-200 border-2 flex items-center">
                     <img src="/assets/images/chunsik_button.png" className="h-full"></img>
-                </button>
+                </Link>
             </div>
 
             {/* divide screen into 3 parts 
@@ -41,22 +57,31 @@ const Chat = () => {
                 3. chat screen
             */}
             {/* part container */}
-            <div className="flex w-full">
-                <div className="flex-col bg-red-500 h-screen w-1/5 border-solid border-r-2 border-slate-950 relative">
+            <div className="flex w-full h-dvh">
+                <div className="flex-col bg-red-500 w-1/5 border-solid border-r-2 border-slate-950 relative">
                     {/* will change this into image-button */}
-                    <button className="px-3 py-3 rounded text-white bg-indigo-700">
+                    <button className="px-3 py-3 rounded text-white bg-indigo-700"
+                    onClick={toggleOptions}>
                         option
                         {/* drop down code goes here */}
                     </button>
 
+                    {/* drop-down menus */}
+                    {showOptions && (
+                        <div className='absolute top-10  bg-white border border-gray-400 px-5 py-5'>
+                            <p className='border-b-2' onClick={() => handleOptionClicks('기부하기')}>기부하기</p>
+                            <p onClick={() => handleOptionClicks('후원하기')}>후원하기</p>
+                        </div>
+                    )}
                     <img src="/assets/images/taemin_musicismylife.jpg" 
                         className="absolute bottom-0"
+                        alt='music is my life'
                     />
                     
 
                 </div>
 
-                <div className="bg-orange-500 h-screen w-2/5 flex-auto flex-col border-solid border-r-2 border-slate-950">
+                <div className="bg-orange-500 w-2/5 flex-auto flex-col border-solid border-r-2 border-slate-950">
                     <h1 className="text-center font-bebas_neue text-6xl">
                         CODE
                     </h1>
@@ -64,15 +89,15 @@ const Chat = () => {
                     
                 </div>
 
-                <div className="bg-yellow-500 h-screen w-2/5 flex-col">
+                <div className="bg-yellow-500 w-2/5 flex-col">
                     <h1 className="text-center font-bebas_neue text-6xl">
                         CHAT
                     </h1>
 
                     {/* texts printed here*/}
-                    <div className="bg-white w-full h-5/6">
+                    <div className="bg-white w-full flex-1 h-5/6">
                         {msg.map((message, index) => (
-                            <Message key={index} text={message.text} sender={message.sender}/>
+                            <Message key={index} {...message}/>
                         ))}
                     </div>
 
@@ -81,6 +106,7 @@ const Chat = () => {
                         <input type="text" placeholder="type here."
                         value={newMessage}
                         onChange={(e) => setNewMessage(e.target.value)}
+                        onKeyDown={handleEnter}
                         className="w-full p-2 border rounded"/>
                         <button 
                         className="bg-blue-500
