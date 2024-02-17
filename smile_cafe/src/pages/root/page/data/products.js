22927afedf2da0f245ef987e7e2102f97b91a11d
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2024-02-18 02:48:29 +0900

    updated file directories
    2nd version coming up

--- smile_cafe/src/pages/root/page/data/products.js
+++ smile_cafe/src/pages/root/page/data/products.js
@@ -1,28 +1,58 @@
-const products = [
-    {
-        id: 1,
-        image: '/images/content/pb_strawberry_latte.jpg',
-        name: '딸기라떼',
-        price: '40',
-    },
-    {
-        id: 2,
-        image: '/images/content/pb_hotchoco.jpg',
-        name: 'Product 2',
-        price: '$12.99',
-    },
-    {
-        id: 3,
-        image: '/images/content/pb_lemonade.jpg',
-        name: 'Product 2',
-        price: '$12.99',
-    },
-    {
-        id: 4,
-        image: '/images/content/pb_icedtea.jpg',
-        name: 'Product 2',
-        price: '$12.99',
-    },
-];
+const products = {
+    //products have 2 big categories(drink, dessert)
+    drinks: [
+        {
+            id: 1,
+            image: '/images/content/drinks/pb_strawberry_latte.jpg',
+            name: '딸기라떼',
+            price: '40',
+        },
+        {
+            id: 2,
+            image: '/images/content/drinks/pb_hotchoco.jpg',
+            name: 'Product 2',
+            price: '$12.99',
+        },
+        {
+            id: 3,
+            image: '/images/content/drinks/pb_lemonade.jpg',
+            name: 'Product 2',
+            price: '$12.99',
+        },
+        {
+            id: 4,
+            image: '/images/content/drinks/pb_icedtea.jpg',
+            name: 'Product 2',
+            price: '$12.99',
+        },
+    ],
+    desserts: [
+        {
+            id: 1,
+            image: '/images/content/desserts/pb_cookie.jpg',
+            name: 'Cookies',
+            price: '10',
+        },
+        {
+            id: 2,
+            image: '/images/content/desserts/pb_financier.jpg',
+            name: 'Financier',
+            price: '30',
+        },
+        {
+            id: 3,
+            image: '/images/content/desserts/pb_macaron.jpg',
+            name: 'Macaron',
+            price: '40',
+        },
+        {
+            id: 4,
+            image: '/images/content/desserts/pb_macaron.jpg',
+            name: 'test',
+            price: '40',
+        },
+    ],
+    
+};
 
 export default products;
--- smile_cafe/src/pages/root/page/index.jsx
+++ smile_cafe/src/pages/root/page/index.jsx
@@ -11,11 +11,11 @@ const Menu = () => {
         {/* Products */}
         <div className="flex flex-wrap justify-center items-center ml-4 mt-10 w-full">
           {
-          products.map(product => (
-            <div key={product.id} className="flex flex-col items-center ml-8 mr-8 mb-4">
-              <img src={product.image} alt={product.name} className="w-24 h-24 object-cover" />
-              <p className="text-3xl font-productFont mt-2">{product.name}</p>
-              <p className="text-2xl font-productFont ">{product.price}</p>
+          products.drinks.map(drink => (
+            <div key={`drink-${drink.id}`} className="flex flex-col items-center ml-8 mr-8 mb-4 mt-6">
+              <img src={drink.image} alt={drink.name} className="w-32 h-32 object-cover" />
+              <p className="text-3xl font-productFont mt-2">{drink.name}</p>
+              <p className="text-2xl font-productFont ">{drink.price}</p>
             </div>
           ))
           }
@@ -26,6 +26,19 @@ const Menu = () => {
       <div className="justify-start items-center flex h-1/3 ml-10 mt-6">
         <span className='text-xl mr-2'>■</span>
         <p className='font-custom text-4xl'>DESSERT</p>
+
+          {/* Products */}
+          <div className="flex flex-wrap justify-center items-center ml-4 mt-10 w-full mr-10">
+            {
+            products.desserts.map(dessert => (
+              <div key={`dessert-${dessert.id}`} className="flex flex-col items-center ml-8 mr-8 mb-4">
+                <img src={dessert.image} alt={dessert.name} className="w-32 h-32 object-cover" />
+                <p className="text-3xl font-productFont mt-2">{dessert.name}</p>
+                <p className="text-2xl font-productFont ">{dessert.price}</p>
+              </div>
+            ))
+            }
+          </div>
       </div>
     </div>
         // <div className="flex-grow justify-between">
